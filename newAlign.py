0248cb94632480354c6ad969370ba67a41a06616
mayaPython <mayaPython@users.noreply.github.com>
2016-04-01 16:41:06 +0100

    Added files via upload

---
+++ newAlign.py
@@ -0,0 +1,36 @@
+'''
+    written by Emily Pollacchi
+    file name newAlign
+    Copyright (C) 2016 by Emily Pollacchi
+    epollacchi@gmail.com
+    Based on a script by Delano Athias
+    www.delanimo.blogspot.co.uk
+    Instructions: first select source object, then select target object.
+
+'''
+import maya.cmds as mc
+
+#use selected objects
+objects = mc.ls(selection = True)
+
+#centre pivot
+mc.xform(objects, cp=True, p=True)
+
+#define a funtion to freeze transformations and delete the history of the objects
+def freezeDelete():
+    mc.makeIdentity(objects, apply = True, t=1, r=1, s=1, n=0)
+    mc.bakePartialHistory(objects, prePostDeformers = True)
+    
+freezeDelete()
+    
+# creates a parent contraint with no offset
+prtCns = mc.parentConstraint()
+
+#deletes the parent constraint
+mc.delete(prtCns)
+
+#clear selection
+mc.select(clear=True)
+
+#freeze transformations and delete the history  
+freezeDelete()
