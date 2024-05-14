536ba2fcf148851d5cc736179b1ecf8ec6f04414
mayaPython <mayaPython@users.noreply.github.com>
2024-05-14 08:14:03 +0100

    Add files via upload

--- newAlign.py
+++ newAlign.py
@@ -1,38 +1,42 @@
 '''
     written by Emily Pollacchi
     file name newAlign
-    Copyright (C) 2016 by Emily Pollacchi
+    Copyright (C) 2024 by Emily Pollacchi
     epollacchi@gmail.com
     Based on a script by Delano Athias
     www.delanimo.blogspot.co.uk
     Instructions: first select source object, then select target object.
 
 '''
-import maya.cmds as mc
+import maya.cmds as cmds
 
-#use selected objects
-objects = mc.ls(selection = True)
+# Use selected objects
+objects = cmds.ls(selection=True)
 
-#centre pivot
-mc.xform(objects, cp=True, p=True)
+# Check for selection
+if not objects:
+    cmds.error("No objects selected.")
 
-#delete the history of the objects
-mc.bakePartialHistory(objects, prePostDeformers = True)
+# Centre pivot
+cmds.xform(objects, cp=True, p=True)
 
-    
-# creates a parent contraint with no offset
-prtCns = mc.parentConstraint()
+# Delete the history of the objects
+cmds.bakePartialHistory(objects, prePostDeformers=True)
 
-#deletes the parent constraint
-mc.delete(prtCns)
+# Creates a parent contraint with no offset
+prtCns = cmds.parentConstraint()
 
-#clear selection
-mc.select(clear=True)
+# Deletes the parent constraint
+cmds.delete(prtCns)
 
-#define a funtion to freeze transformations and delete the history of the objects
+# Clear selection
+cmds.select(clear=True)
+
+
+# Freeze transformations and delete the history of the objects
 def freezeDelete():
-    mc.makeIdentity(objects, apply = True, t=1, r=1, s=1, n=0)
-    mc.bakePartialHistory(objects, prePostDeformers = True)
-    
+    cmds.makeIdentity(objects, apply=True, t=1, r=1, s=1, n=0)
+    cmds.bakePartialHistory(objects, prePostDeformers=True)
+
 
 freezeDelete()
